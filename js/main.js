c706fd32bbe2a35a1324fa1414254900b7d478a4
Federico Daniel Rondanini <fede.ronda@gmail.com>
2023-03-31 14:46:17 -0300

    botones agregar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,45 @@
 const ProductosArray = [
     {
-        id: "buzo-06",
+        id: "buzo-01",
+        titulo: "Buzo Azul",
+        imagen: "./img/indumentaria/buzo-azul.png",
+        marca: "DIABLXS",
+        talle: "M al XL",
+        efectivo: "10%",
+        categoria: {
+            nombre: "indumentaria",
+            id: "indumentaria"
+        },
+        precio: 5999
+    },
+    {
+        id: "buzo-02",
+        titulo: "Buzo Blanco",
+        imagen: "./img/indumentaria/buzo-blanco.png",
+        marca: "DIABLXS",
+        talle: "M al XL",
+        efectivo: "10%",
+        categoria: {
+            nombre: "indumentaria",
+            id: "indumentaria"
+        },
+        precio: 5999
+    },
+    {
+        id: "buzo-03",
+        titulo: "Buzo Negro",
+        imagen: "./img/indumentaria/buzo-negro.png",
+        marca: "DIABLXS",
+        talle: "M al XL",
+        efectivo: "10%",
+        categoria: {
+            nombre: "indumentaria",
+            id: "indumentaria"
+        },
+        precio: 5999
+    },
+    {
+        id: "buzo-04",
         titulo: "Buzo Rojo",
         imagen: "./img/indumentaria/buzo-rojo.png",
         marca: "DIABLXS",
@@ -13,9 +52,9 @@ const ProductosArray = [
         precio: 5999
     },
     {
-        id: "buzo-06",
-        titulo: "Buzo Rojo",
-        imagen: "./img/indumentaria/buzo-rojo.png",
+        id: "buzo-05",
+        titulo: "Buzo Verde",
+        imagen: "./img/indumentaria/buzo-verde.png",
         marca: "DIABLXS",
         talle: "M al XL",
         efectivo: "10%",
@@ -27,47 +66,8 @@ const ProductosArray = [
     },
     {
         id: "buzo-06",
-        titulo: "Buzo Rojo",
-        imagen: "./img/indumentaria/buzo-rojo.png",
-        marca: "DIABLXS",
-        talle: "M al XL",
-        efectivo: "10%",
-        categoria: {
-            nombre: "indumentaria",
-            id: "indumentaria"
-        },
-        precio: 5999
-    },
-    {
-        id: "buzo-06",
-        titulo: "Buzo Rojo",
-        imagen: "./img/indumentaria/buzo-rojo.png",
-        marca: "DIABLXS",
-        talle: "M al XL",
-        efectivo: "10%",
-        categoria: {
-            nombre: "indumentaria",
-            id: "indumentaria"
-        },
-        precio: 5999
-    },
-    {
-        id: "buzo-06",
-        titulo: "Buzo Rojo",
-        imagen: "./img/indumentaria/buzo-rojo.png",
-        marca: "DIABLXS",
-        talle: "M al XL",
-        efectivo: "10%",
-        categoria: {
-            nombre: "indumentaria",
-            id: "indumentaria"
-        },
-        precio: 5999
-    },
-    {
-        id: "buzo-06",
-        titulo: "Buzo Rojo",
-        imagen: "./img/indumentaria/buzo-rojo.png",
+        titulo: "Buzo Violeta",
+        imagen: "./img/indumentaria/buzo-violeta.png",
         marca: "DIABLXS",
         talle: "M al XL",
         efectivo: "10%",
@@ -80,6 +80,8 @@ const ProductosArray = [
 ];
 
 const ContenedorProductos = document.querySelector("#contenedor-productos");
+let BotonesAgregar = document.querySelectorAll(".producto-agregar");
+
 
 function CargarProductos() {
 
@@ -94,13 +96,19 @@ function CargarProductos() {
                 <span class="tarjeta__descrip">Talle: <strong>${producto.talle}</strong></span>
                 <span class="tarjeta__descrip">Precio: <strong>$${producto.precio}</strong></span>
                 <span class="tarjeta__descrip">Efectivo: <strong>${producto.efectivo} OFF</strong> </span>
-                <a class="btn-comprar" href="" id="${producto.id}">Comprar</a>
+                <a class="btn-comprar producto-agregar" id="${producto.id}">Agregar</a>
         `;
 
         ContenedorProductos.append(article);
     })
 
+    ActualizarBotonesAgregar();
+
 }
 
 CargarProductos();
+
+function ActualizarBotonesAgregar() {
+    BotonesAgregar = document.querySelectorAll(".producto-agregar");
+}
 
